f1eea2367c6728d12b341a7a54bc0a7bf52dd0b0
lucasmullers <lucas.mullers@gmail.com>
2024-04-03 10:48:09 -0300

    feat: add new asset to extract bens candidatos

--- scrapy_govbr_data/scrapy_govbr_data/assets/extract_asset.py
+++ scrapy_govbr_data/scrapy_govbr_data/assets/extract_asset.py
@@ -94,3 +94,57 @@ def extract_candidatos() -> None:
                 rmtree(f"./{file_name}")
         except Exception as e:
             print(f"Não foi possível coletar o arquivo: {e}")
+
+
+@asset(group_name="scrapy_tse_data")
+def extract_bens_candidatos() -> None:
+    """
+    Function to extract candidatos data from TSE website and upload it to S3.
+    """
+    start_date = 2000
+    end_date = datetime.now().year + 1
+    s3 = S3()
+
+    for year in range(start_date, end_date, 2):
+        url = f"https://cdn.tse.jus.br/estatistica/sead/odsele/bem_candidato/bem_candidato_{year}.zip"
+        file_name = f"bem_candidato_{year}"
+        zip_file = f"{file_name}.zip"
+
+        logger.info("Downloading candidates from {} election.".format(year))
+
+        try:
+            resp = request.urlretrieve(url, zip_file)
+
+            logger.info(f"Response received: {resp}")
+
+            with zipfile.ZipFile(f"./{zip_file}", "r") as zip_ref:
+                zip_ref.extractall(f"./{file_name}")
+
+            if path.exists(zip_file):
+                logger.info(f"Deleting zip file: {zip_file}")
+                remove(zip_file)
+
+            for uf in UFS:
+                logger.info(f"Converting encoding from file: {file_name}_{uf}.csv")
+                df = pd.read_csv(
+                    f"./{file_name}/{file_name}_{uf}.csv", encoding="latin-1", sep=";"
+                )
+                df.to_csv(
+                    f"./{file_name}/{file_name}_{uf}.csv",
+                    encoding="utf-8",
+                    sep=";",
+                    index=False,
+                )
+
+                logger.info(f"Uploading file '{file_name}_{uf}.csv'to S3.")
+                s3.upload_file(
+                    file_path=f"./{file_name}/{file_name}_{uf}.csv",
+                    bucket_name="govbr-data",
+                    object_name=f"LANDING/TSE/BENS_CANDIDATOS/{file_name}_{uf}.csv",
+                )
+
+            if path.exists(f"./{file_name}"):
+                logger.info(f"Deleting folder: {file_name}")
+                rmtree(f"./{file_name}")
+        except Exception as e:
+            print(f"Não foi possível coletar o arquivo: {e}")
